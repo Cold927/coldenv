0e2a721efd6f8d8a51ef486125a463cc9073394c
Roman Cold <colddevelop@gmail.com>
2022-05-27 12:45:16 +0300

    Uncomment lines

--- src/playgrounds/decoder.ts
+++ src/playgrounds/decoder.ts
@@ -2,9 +2,9 @@ import * as fs from 'fs';
 import * as CryptoJS from 'crypto-js';
 
 export const main = async (moduleRef, args: string[]) => {
-  // const privateKey = fs.readFileSync(__dirname + '/../../private.key', 'utf-8');
+  const privateKey = fs.readFileSync(__dirname + '/../../private.key', 'utf-8');
   const token = fs.readFileSync(__dirname + '/../../secret.txt', 'utf-8');
-  let bytes = CryptoJS.AES.decrypt(token, '');
+  let bytes = CryptoJS.AES.decrypt(token, privateKey);
   let decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
   fs.writeFileSync('development.json', decryptedData);
 };
